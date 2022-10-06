6995f83a1d2aae1c985ea56362062883cdfdd5e6
MrGansoStr <ccounttnro1@gmail.com>
2022-10-06 00:08:13 -0500

    altura video rickroll

--- src/components/blog/Videotutorials.jsx
+++ src/components/blog/Videotutorials.jsx
@@ -2,7 +2,7 @@ import ReactPlayer from 'react-player'
 export const Videotutorials = () => {
   return (
     <>
-    <div className="container-xl">
+    <div className="container-xl h-auto">
       <ReactPlayer url={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
       width="100%" controls/>
     </div>
